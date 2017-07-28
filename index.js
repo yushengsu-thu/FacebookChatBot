3930454f7d0b54aa83e4fffa368d2311007de5c4
Sheng <ky200120000@gmail.com>
2017-07-28 16:15:18 +0800

    Message Structure

--- index.js
+++ index.js
@@ -46,43 +46,42 @@ function sendText(sender, text) {
 	//let messageData = {text: text}
 	////
     let messageData = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-            }]
-        }
+        attachment: {
+            type: "template",
+            payload: {
+                template_type: "generic",
+                elements: [{
+                    title: "rift",
+                    subtitle: "Next-generation virtual reality",
+                    item_url: "https://www.oculus.com/en-us/rift/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                    buttons: [{
+                        type: "web_url",
+                        url: "https://www.oculus.com/en-us/rift/",
+                        title: "Open Web URL"
+                    }, {
+                        type: "postback",
+                        title: "Call Postback",
+                        payload: "Payload for first bubble",
+                    }],
+                }, {
+                    title: "touch",
+                    subtitle: "Your Hands, Now in VR",
+                    item_url: "https://www.oculus.com/en-us/touch/",               
+                    image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                    buttons: [{
+                        type: "web_url",
+                        url: "https://www.oculus.com/en-us/touch/",
+                        title: "Open Web URL"
+                    }, {
+                        type: "postback",
+                        title: "Call Postback",
+                        payload: "Payload for second bubble",
+                    }]
+                }]
+            }
         }
     };  
-	console.log(messageData)
     ////
     request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
