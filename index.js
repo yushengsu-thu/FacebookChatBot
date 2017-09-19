fa1f8931c313dd719c40145b7f5274faf7832d3e
Sheng <ky200120000@gmail.com>
2017-09-19 11:35:42 +0800

    update

--- index.js
+++ index.js
@@ -142,43 +142,43 @@ function sendText(sender, text) {  //sendText ==> sendMessage
         }
 
         ////
-        "persistent_menu":[
+        persistent_menu:[
   {
-    "locale":"default",
-    "composer_input_disabled": true,
-    "call_to_actions":[
+    locale:"default",
+    composer_input_disabled: true,
+    call_to_actions:[
       {
-        "title":"My Account",
-        "type":"nested",
-        "call_to_actions":[
+        title:"My Account",
+        type:"nested",
+        call_to_actions:[
           {
-            "title":"Pay Bill",
-            "type":"postback",
-            "payload":"PAYBILL_PAYLOAD"
+            title:"Pay Bill",
+            type:"postback",
+            payload:"PAYBILL_PAYLOAD"
           },
           {
-            "title":"History",
-            "type":"postback",
-            "payload":"HISTORY_PAYLOAD"
+            title:"History",
+            type:"postback",
+            payload:"HISTORY_PAYLOAD"
           },
           {
-            "title":"Contact Info",
-            "type":"postback",
-            "payload":"CONTACT_INFO_PAYLOAD"
+            title:"Contact Info",
+            type:"postback",
+            payload:"CONTACT_INFO_PAYLOAD"
           }
         ]
       },
       {
-        "type":"web_url",
-        "title":"Latest News",
-        "url":"http://petershats.parseapp.com/hat-news",
-        "webview_height_ratio":"full"
+        type:"web_url",
+        title:"Latest News",
+        url:"http://petershats.parseapp.com/hat-news",
+        webview_height_ratio:"full"
       }
     ]
   },
   {
-    "locale":"zh_CN",
-    "composer_input_disabled":false
+    locale:"zh_CN",
+    composer_input_disabled:false
   }
 ]
         ////
