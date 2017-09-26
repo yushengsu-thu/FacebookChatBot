26c3d97046b9e2475dc2112f77a1d9f9f219b900
Sheng <ky200120000@gmail.com>
2017-09-26 11:24:52 +0800

    v

--- index.js
+++ index.js
@@ -160,7 +160,6 @@ function browseAirticle(sender, text) {  //browseAirticle ==> sendMessage
                         webview_height_ratio: "full" //compact, tall, full
 	                    },{
 	                        type:"element_share",
-                            elements:[{title:"分享"}],
 	                    },{
 	                        type: "postback",
 	                        title: "回首頁",
