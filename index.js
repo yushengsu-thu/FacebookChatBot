ca0ad2d08e56a377e4b540278f2658f5c9fd3a9f
Sheng <ky200120000@gmail.com>
2017-10-01 15:24:02 +0800

    v

--- index.js
+++ index.js
@@ -274,9 +274,9 @@ function browseAirticle(sender, text) {  //browseAirticle ==> sendMessage
 		}
         /*Restore data*/
         const fs = require('fs');
-        const content = body;
-        //const content = JSON.stringify(body);
-        //console.log(content)
+        //const content = body;
+        const content = JSON.stringify(body);
+        console.log(content)
 
         fs.writeFile("userdata.json", content, 'utf8', function (err) {
         if (err) {
