a78d7d1feb4f31777fcc97d673f0b02cc79143cd
Sheng <ky200120000@gmail.com>
2017-10-01 21:26:54 +0800

    v

--- index.js
+++ index.js
@@ -84,17 +84,17 @@ function checkStocklist(sender, text){
     //var brands_ans_photos = JSON.parse(fs.readFileSync('brands_ans_photos.json', 'utf8'));
 
     var messageData = {
-        text: "我們列出部分美股如下，你也可以點選‘更多’來找尋你感興趣的公司",
+        //text: "我們列出部分美股如下，你也可以點選‘更多’來找尋你感興趣的公司",
         quick_replies:[{
             content_type:"text",
             title:"3M",
-            //image_url:"https://stockfeel-1.azureedge.net/wp-content/themes/stockfeel_2016_theme/images/stock_company/usa/logo_stock-usa-3m.svg",
+            image_url:"https://stockfeel-1.azureedge.net/wp-content/themes/stockfeel_2016_theme/images/stock_company/usa/logo_stock-usa-3m.svg",
             payload:"3M"
         }]
     }
 
     request({
-		url: "https://graph.facebook.com/v2.6/me/messages?",
+		url: "https://graph.facebook.com/v2.6/me/messages",
         qs : {access_token: token},
 		method: "POST",
 		json: {
