61c5f8527d09cf66c6a9430944f49031d66de3ec
Sheng <ky200120000@gmail.com>
2017-10-02 10:48:31 +0800

    v

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function checkStocklist(sender, text){
         })
     }
         
-    //console.log(data)
+    console.log(data)
 
     var messageData = {
         text: "我們列出部分美股如下，你也可以點選‘更多’來找尋你感興趣的公司",
